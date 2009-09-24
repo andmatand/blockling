1bf11b9afe36ec52c62723bdcf80b0248ce879a5
andrew <andrew@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2009-09-24 08:29:14 +0000

    -Got win32 compilation to work again!! =D  Praise YHWH

--- win32/make-win32.sh
+++ win32/make-win32.sh
@@ -3,12 +3,12 @@
 
 #/opt/mingw/usr/bin/i386-mingw32msvc-g++ -o blockman.exe ../main.cpp $CFLAGS $LIBS
 
-/opt/mingw/usr/bin/i386-mingw32msvc-g++ -o blockman.exe ../main.cpp -D_GNU_SOURCE=1 -Dmain=SDL_main -I/opt/mingw/usr/i386-mingw32msvc/include/SDL -L/opt/mingw/usr/i386-mingw32msvc/lib -lmingw32 -lSDLmain -lSDL_mixer -lSDL -lwinmm -mwindows
+/opt/mingw/usr/bin/i386-mingw32msvc-g++ -o blockman.exe ../main.cpp -D_GNU_SOURCE=1 -Dmain=SDL_main -I/opt/mingw/usr/i386-mingw32msvc/include/SDL -L/opt/mingw/usr/i386-mingw32msvc/lib -lmingw32 -lSDLmain SDL.dll SDL_mixer.dll -lwinmm -mwindows 
 
 if [ -f blockman.exe ]; then
-	/opt/mingw/usr/bin/strip blockman.exe
+	/opt/mingw/usr/bin/i386-mingw32msvc-strip blockman.exe
 	
 	rm blockman-win32.zip
-	zip -r blockman-win32 blockman.exe COPYING data/ README-SDL.txt SDL.dll -x \*svn
+	zip -r blockman-win32 blockman.exe COPYING data/ README-SDL.txt SDL.dll SDL_mixer.dll -x \*svn
 	rm blockman.exe
 fi
