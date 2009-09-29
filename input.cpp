d462c0b808ca5645d0afa42f592b7e1d591fb66e
andrew <andrew@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2009-09-29 20:30:24 +0000

    -Added some more keynames -Added a new level

--- input.cpp
+++ input.cpp
@@ -360,6 +360,139 @@ const char* KeyName(SDLKey sym) {
 		case SDLK_SPACE:
 			return "SPACE";
 			break;
+		case SDLK_HOME:
+			return "HOME";
+			break;
+		case SDLK_END:
+			return "END";
+			break;
+
+		case SDLK_1:
+			return "1";
+			break;
+		case SDLK_2:
+			return "2";
+			break;
+		case SDLK_3:
+			return "3";
+			break;
+		case SDLK_4:
+			return "4";
+			break;
+		case SDLK_5:
+			return "5";
+			break;
+		case SDLK_6:
+			return "6";
+			break;
+		case SDLK_7:
+			return "7";
+			break;
+		case SDLK_8:
+			return "8";
+			break;
+		case SDLK_9:
+			return "9";
+			break;
+		case SDLK_0:
+			return "0";
+			break;
+
+		case SDLK_SEMICOLON:
+			return ";";
+			break;
+		case SDLK_QUOTE:
+			return "'";
+			break;
+		case SDLK_COMMA:
+			return ",";
+			break;
+		case SDLK_PERIOD:
+			return ".";
+			break;
+		case SDLK_SLASH:
+			return "/";
+			break;
+
+
+		case SDLK_a:
+			return "a";
+			break;
+		case SDLK_b:
+			return "b";
+			break;
+		case SDLK_c:
+			return "c";
+			break;
+		case SDLK_d:
+			return "d";
+			break;
+		case SDLK_e:
+			return "e";
+			break;
+		case SDLK_f:
+			return "f";
+			break;
+		case SDLK_g:
+			return "g";
+			break;
+		case SDLK_h:
+			return "h";
+			break;
+		case SDLK_i:
+			return "i";
+			break;
+		case SDLK_j:
+			return "j";
+			break;
+		case SDLK_k:
+			return "k";
+			break;
+		case SDLK_l:
+			return "l";
+			break;
+		case SDLK_m:
+			return "m";
+			break;
+		case SDLK_n:
+			return "n";
+			break;
+		case SDLK_o:
+			return "o";
+			break;
+		case SDLK_p:
+			return "p";
+			break;
+		case SDLK_q:
+			return "q";
+			break;
+		case SDLK_r:
+			return "r";
+			break;
+		case SDLK_s:
+			return "s";
+			break;
+		case SDLK_t:
+			return "t";
+			break;
+		case SDLK_u:
+			return "u";
+			break;
+		case SDLK_v:
+			return "v";
+			break;
+		case SDLK_w:
+			return "w";
+			break;
+		case SDLK_x:
+			return "x";
+			break;
+		case SDLK_y:
+			return "y";
+			break;
+		case SDLK_z:
+			return "z";
+			break;
 		default:
 			break;
 	}
