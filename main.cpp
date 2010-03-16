f3e14f35b39020200d35c781aec7c03d9626bc97
andrew <andrew@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2010-03-16 01:40:54 +0000

    Replaced all occurrences of "Blockman" with "Blockling" (using a handy little Perl script)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 /*
  *   Copyright 2009 Andrew Anderson <www.billamonster.com>
  *      
- *   This file is part of Blockman.
+ *   This file is part of Blockling.
  *
- *   Blockman is free software: you can redistribute it and/or modify
+ *   Blockling is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   Blockman is distributed in the hope that it will be useful,
+ *   Blockling is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.*
  *
  *   You should have received a copy of the GNU General Public License
- *   along with Blockman.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with Blockling.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
