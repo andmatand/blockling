7a20f35b62ffb3adb786c109088807dc0ccd3df8
andreanadelphon <andreanadelphon@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2010-03-20 01:13:16 +0000

    Removed information about my local win32 build directory that is not (presently) included in the svn tree

--- tar-it-up.sh
+++ tar-it-up.sh
@@ -1,3 +1,2 @@
 # This script creates a source archive for public release
-# For a win32 binary release, see the script in win32/
 tar -czf blockling.tar.gz --exclude=blockling.tar.gz --exclude=win32 --exclude=.svn --exclude tar-it-up.sh *
