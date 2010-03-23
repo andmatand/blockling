24a154b23222423ebf493e9654e389da98ed56c9
andreanadelphon <andreanadelphon@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2010-03-23 05:38:47 +0000

    -Added Peter Norland to credits -Added automatic upload to tar script

--- tar-it-up.sh
+++ tar-it-up.sh
@@ -1,2 +1,6 @@
 # This script creates a source archive for public release
-tar -czf blockling-src-r$(svnversion | cut -d ":" -f 2 | egrep "[0-9]+" -o).tar.gz --exclude=blockling.tar.gz --exclude=blockling --exclude=win32 --exclude=.svn --exclude tar-it-up.sh *
+filename=blockling-src-r$(svnversion | cut -d ":" -f 2 | egrep "[0-9]+" -o).tar.gz
+tar -czf $filename --exclude=blockling.tar.gz --exclude=blockling --exclude=win32 --exclude=.svn --exclude tar-it-up.sh *
+
+# Upload it to sourceforge
+scp $filename andreanadelphon,blockling@frs.sourceforge.net:/home/frs/project/b/bl/blockling/
