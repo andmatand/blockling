cc79d460c3b77f41c510ff64f1eb72a27737b83e
andreanadelphon <andreanadelphon@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2010-03-24 05:59:42 +0000

    -Modified tar script

--- tar-it-up.sh
+++ tar-it-up.sh
@@ -1,6 +1,6 @@
 # This script creates a source archive for public release
 filename=blockling-src-r$(svnversion | cut -d ":" -f 2 | egrep "[0-9]+" -o).tar.gz
-tar -czf $filename --exclude=blockling.tar.gz --exclude=blockling --exclude=win32 --exclude=.svn --exclude tar-it-up.sh *
+tar -czf $filename --exclude=blockling*.tar.gz --exclude=blockling --exclude-vcs --exclude=.* --exclude tar-it-up.sh *
 
 # Upload it to sourceforge
 scp $filename andreanadelphon,blockling@frs.sourceforge.net:/home/frs/project/b/bl/blockling/
