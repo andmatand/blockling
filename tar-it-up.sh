75b6ccaf959262c4c8e5b930e29418e13e821f18
andreanadelphon <andreanadelphon@d0a4e4f8-4781-4f5b-8b1a-b538447907f4>
2010-03-24 17:50:03 +0000

    -Improved tar script

--- tar-it-up.sh
+++ tar-it-up.sh
@@ -1,6 +1,13 @@
 # This script creates a source archive for public release
+
+# Delete previous tarballs
+rm blockling-src*.tar.gz
+
+# Construct the filename from current svn revision number
 filename=blockling-src-r$(svnversion | cut -d ":" -f 2 | egrep "[0-9]+" -o).tar.gz
-tar -czf $filename --exclude=blockling*.tar.gz --exclude=blockling --exclude-vcs --exclude=.* --exclude tar-it-up.sh *
+
+# Make the current tarball
+tar -czf $filename --exclude=blockling*.tar.gz --exclude=blockling --exclude-vcs --exclude=.* --exclude=win32 --exclude tar-it-up.sh *
 
 # Upload it to sourceforge
 scp $filename andreanadelphon,blockling@frs.sourceforge.net:/home/frs/project/b/bl/blockling/
