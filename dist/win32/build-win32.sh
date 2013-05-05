a523c5ba16539088d53e7c2102bc8d0637493c06
Andrew Anderson <andrewloveskitties@gmail.com>
2013-05-05 00:02:40 -0700

    Added win32 distribution script

---
+++ dist/win32/build-win32.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script will create a distributable zip file of blockling for Windows
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <path/to/MXE>"
+    echo "    Get MXE cross-compiling environment here: http://mxe.cc/"
+    exit -1
+fi
+
+ZIPDIR=blockling-win32
+ZIPFILE=blockling-win32.zip
+
+# Delete the old zip file
+rm -f $ZIPFILE
+
+# Compile blockling.exe
+ln -s ../../data/ ./data
+make MXE_PATH=$1
+rm data
+
+# Create a directory to be zipped
+mkdir $ZIPDIR
+
+# Copy/move necessary files into the zip directory
+cp -r ../../data ../../README.md ../../COPYING *.dll *.txt $ZIPDIR
+mv blockling.exe $ZIPDIR
+
+# Convert textfile linebreaks to Windows linebreaks and file extensions
+perl -p -i -e 's/\n/\r\n/' $ZIPDIR/README.md
+mv $ZIPDIR/README.md $ZIPDIR/readme.txt
+perl -p -i -e 's/\n/\r\n/' $ZIPDIR/COPYING
+mv $ZIPDIR/COPYING $ZIPDIR/license.txt
+
+# Zip the directory
+zip -r $ZIPFILE $ZIPDIR
+
+# Delete the zip directory
+rm -rf $ZIPDIR
