08d18de23fc364d5115e5c5b11eda89848a1af09
Andrew Anderson <andrewloveskitties@gmail.com>
2013-12-01 22:56:21 -0800

    Added .opk build script

---
+++ dist/gcw0/build-opk.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+DATADIR=usr/local/games/blockling
+
+# Compile the executable and then come back to this directory
+cd ../..
+make DATADIR=$DATADIR/
+mipsel-linux-strip blockling
+cd -
+
+# Create a temp directory with the necessary package files
+TEMPDIR=package
+mkdir $TEMPDIR
+cp default.gcw0.desktop $TEMPDIR
+cp icon.png $TEMPDIR
+cp ../../blockling $TEMPDIR
+mkdir -p $TEMPDIR/$DATADIR
+rsync -av --exclude='*.xcf' ../../data/* $TEMPDIR/$DATADIR
+
+# Create the .opk package
+mksquashfs $TEMPDIR blockling.opk -all-root -noappend -no-exports -no-xattrs
+
+# Delete the temp directory
+rm -rf $TEMPDIR
